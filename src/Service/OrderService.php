9cb21275c86e339d85249c861adbeb0582592323
Benjamin Nussbaum <bnussbau@users.noreply.github.com>
2023-06-05 21:45:57 +0200

    feat: rename to ERIVE.delivery; first prerelease

--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -34,11 +34,10 @@ class OrderService
     ) {
         $this->systemConfigService = $systemConfigService;
         $this->orderRepository = $orderRepository;
-
-        $this->gthEnv = $systemConfigService->get('GreenToHome.config.gthEnvironment');
-        $this->apiKey = $systemConfigService->get('GreenToHome.config.apikey');
-        $this->customParcelIdField = $systemConfigService->get('GreenToHome.config.parcelIdFieldName') ?? 'custom_gth_ParcelID';
-        $this->customStickerUrlField = $systemConfigService->get('GreenToHome.config.stickerUrlFieldName') ?? 'custom_gth_StickerUrl';
+        $this->gthEnv = $systemConfigService->get('EriveDelivery.config.gthEnvironment');
+        $this->apiKey = $systemConfigService->get('EriveDelivery.config.apikey');
+        $this->customParcelIdField = $systemConfigService->get('EriveDelivery.config.parcelIdFieldName') ?? 'custom_gth_ParcelID';
+        $this->customStickerUrlField = $systemConfigService->get('EriveDelivery.config.stickerUrlFieldName') ?? 'custom_gth_StickerUrl';
 
         $this->context = Context::createDefaultContext();
     }
@@ -117,7 +116,7 @@ class OrderService
         $customer = new Customer(); // \GreenToHome\Model\Customer
         $customer->setName($order->getOrderCustomer()->getFirstName() . ' ' . $order->getOrderCustomer()->getLastName());
         $customer->setEmail($order->getOrderCustomer()->getEmail());
-        $customer->setPhone($shippingAddress->getPhoneNumber());
+        $customer->setPhone($shippingAddress->getPhoneNumber() ?: "0");
 
         $customerAddress = new Address(); // \GreenToHome\Model\Address
         $customerAddress->setCountry($shippingAddress->getCountry()->getIso());
