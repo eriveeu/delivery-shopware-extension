1b40676982e69a38a8e139119f86a7011d9e15fa
VPysarenko-nme <vladyslav.pysarenko@nm-e.com>
2023-11-05 23:58:21 +0100

    remove comments

--- src/Listener/OrderListener.php
+++ src/Listener/OrderListener.php
@@ -5,23 +5,19 @@ declare(strict_types=1);
 namespace Erive\Delivery\Listener;
 
 use Erive\Delivery\Service\OrderService;
-// use Psr\Log\LoggerInterface;
 use Shopware\Core\Checkout\Order\Event\OrderStateMachineStateChangeEvent;
 use Shopware\Core\Framework\DataAbstractionLayer\Event\EntityWrittenEvent;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
 
 class OrderListener
 {
-    // protected $logger;
     protected SystemConfigService $systemConfigService;
     protected OrderService $orderService;
 
     public function __construct(
-        // LoggerInterface $logger,
         SystemConfigService $systemConfigService,
         OrderService $orderService
     ) {
-        // $this->logger = $logger;
         $this->systemConfigService = $systemConfigService;
         $this->orderService = $orderService;
     }
